2da0cea38b06183a4c0a01cfd10c4b351f96ac94
Jenny Gebske <jennifer@gebske.de>
2017-10-03 23:40:23 +0200

    test online/offline status

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,23 @@
++function() {
+  'use strict';
+  var isOnline;
+  var stat = document.getElementById('status');
+  var amIOnline = function() {
+    stat.textContent = navigator.online ? 'online' : 'offline';
+  }
+  window.addEventListener('online', amIOnline);
+  window.addEventListener('offline', amIOnline);
+  amIOnline();
+}();
+
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('./service-worker.js')
     .then(function() {
-      console.log('Service Worker Registered');
+      console.log('Service Worker registered');
+    })
+    .catch(function(err) {
+      console.log('Service Worker not registered', err);
     });
 }
