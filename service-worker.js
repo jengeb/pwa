5a3605ddcaac742c8a5a278f57482c8b7923d40f
Jenny Gebske <jennifer@gebske.de>
2017-10-04 08:12:49 +0200

    use arrow funtions

--- service-worker.js
+++ service-worker.js
@@ -8,21 +8,28 @@ const filesToCache = [
   '/styles/inline.css'
 ];
 
-self.addEventListener('install', function(e) {
+// self: (=! window) ServiceWorkerGlobalScope object
+self.addEventListener('install', e => {
   console.log('[ServiceWorker] Install');
+  // wait for ServiceWorker when calls are asynchronous
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    // Chrome: ServiceWorker/CacheStorage
+    // Firefox: default cache
+    // returns a promise to thr opened cache object
+    caches.open(cacheName).then(cache => {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
+    }))
     })
   );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
+    caches.keys().then(keyList => {
+      // keyList.forEach(key => {}) // alternative
+      return Promise.all(keyList.map(key => {
         if (key !== cacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
@@ -33,10 +40,10 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
