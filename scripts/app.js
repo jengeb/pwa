50da8a303dbd4529b88e2212ce711bcace11165e
Jenny Gebske <jennifer@gebske.de>
2017-10-08 00:00:03 +0200

    fix bug

--- scripts/app.js
+++ scripts/app.js
@@ -13,19 +13,19 @@
 
 // register JS file containing access to Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./service-worker.js')
-    .then(function() {
-      console.log('Service Worker registered');
+  navigator.serviceWorker.register('./service-worker.js').then(
+    (serviceWorkerRegistration) => {
+      serviceWorkerRegistration.pushManager.subscribe().then(
+        (pushSubscription) => {
+          // TODO
+          // endpoint to send push notifications
+          console.log(pushSubscription.subscriptionId);
+          console.log(pushSubscription.endpoint);
+        });
     })
     .catch(function(err) {
       console.log('Service Worker not registered', err);
     });
 
-  serviceWorkerRegistration.pushManager
-    .subscribe()
-    .then((endpoint) => {
-      // TODO
-      // endpoint to send push notifications
-    });
+
 }
