bd9dbb0ca873897465f20b1761f5bb223477324d
Jenny Gebske <jennifer@gebske.de>
2017-10-10 19:24:03 +0200

    remove unused code

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,3 @@
-+function() {
-  'use strict';
-  var isOnline;
-  var stat = document.getElementById('status');
-  var amIOnline = function() {
-    stat.textContent = navigator.online ? 'online' : 'offline';
-  }
-  window.addEventListener('online', amIOnline);
-  window.addEventListener('offline', amIOnline);
-  amIOnline();
-}();
-
-
 // register JS file containing access to Service Worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js').then(
